af1d003f6bae63c5b43aa9593336725cfa041f03
AndreiC805 <condria.razvan@gmail.com>
2025-11-20 13:16:30 +0000

    redefined classes alongside modifying code line

--- src/class.kt
+++ src/class.kt
@@ -1,28 +1,17 @@
-class Destination(val name: String, val singleFare: Double, val returnFare: Double) {
-    var totalTakings: Double = 0.0
-}
-
-// Represents a ticket
-class Ticket(
-    val origin: String,
-    val destination: String,
-    val ticketType: String, // "Single" or "Return"
-    val price: Double
-) {
-    fun printTicket() {
-        println("***")
-        println("$origin to $destination")
-        println("Price: £${"%.2f".format(price)} [$ticketType]")
-        println("***")
-    }
-}
-
-// Represents the main ticket machine
-class TicketMachine(val originStation: String) {
-    private val destinations = listOf(
-        Destination("London", 25.00, 45.00),
-        Destination("Manchester", 30.00, 55.00),
-        Destination("Birmingham", 20.00, 35.00),
-        Destination("Liverpool", 28.00, 50.00),
-        Destination("Leeds", 27.50, 49.00)
+class Tiket {
+    data class Destination(
+        val name: String,
+        val singlePrice: Double,
+        val returnPrice: Double,
+        var totalTakings: Double = 0.0
     )
+
+    class TicketMachine(private val originStation: String) {
+        private val destinations = mutableListOf(
+            Destination("London", 45.0, 60.0),
+            Destination("Cardiff", 15.0, 25.0),
+            Destination("Weston Super Mare", 20.0, 35.0),
+            Destination("Swansea", 35.0, 55.0)
+        )
+
+        private var insertedMoney: Double = 0.0
